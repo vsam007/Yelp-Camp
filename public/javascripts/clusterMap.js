939fd351bb0a4100b179d40d0daf2ec913c61005
Vaibhav Jain <53565053+vsam007@users.noreply.github.com>
2023-12-28 20:32:55 +0530

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/dark-v11',
-    center: [61.0008497,19.7296964]
+    center: [78.9629,20.5937]
     
     zoom: 3
 });
